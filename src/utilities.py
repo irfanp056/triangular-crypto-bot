0b925e21a7a444a9060cd27c5666d5ace674f8d8
Akira <luckyanchor2@gmail.com>
2021-01-05 01:47:11 -0800

    Added more utilities

--- src/utilities.py
+++ src/utilities.py
@@ -98,3 +98,24 @@ def get_precision(pairs):
         precisions.append(precision)
     print("precisions: %s" % precisions)
     return precisions
+
+def print_all_pairs():
+    r = requests.get("https://api.binance.com/api/v3/ticker/price")
+    print(r.json())
+
+def price(pair):
+    r = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={pair}")
+    return r.json()
+
+def price_change(pair):
+    p = price(pair)
+    print(p)
+    while True:
+        num = price(pair)
+        if num != p:
+            r = num-p
+            change = f"{r:.9f}"
+            t = time.time()
+            print(f"Price: {num} | Change: {change} | Time (s): {t}")
+            p = num
+    return ""
